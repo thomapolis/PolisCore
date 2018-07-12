db8f96c21cb2ec7b825208393172aa64b0a24ffa
thomapolis <thomapolis@gmail.com>
2018-07-12 18:50:26 +0200

    Work on sql

---
+++ src/database/SqlConnection.java
@@ -0,0 +1,50 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SqlConnection {
+
+	private Connection connection;
+	private String urlbase,host,database,user,pass;
+	
+	public SqlConnection(String urlbase, String host, String database, String user, String pass) {
+		
+		this.urlbase = urlbase;
+		this.host = host;
+		this.database = database;
+		this.user = user;
+		this.pass = pass;
+		
+	}
+
+	public void connect() {
+		
+		try {
+			connection = DriverManager.getConnection(urlbase+host+"/" + database, user, pass);
+			System.out.println("connected !");
+		} catch (SQLException e) {
+			System.out.println("connection error !!!");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void disconnect() {
+		
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+
+	public boolean isConnected() {
+		
+		return connection != null;
+	}
+	
+}
--- src/fr/thomapolis/poliscore/PolisCore.java
+++ src/fr/thomapolis/poliscore/PolisCore.java
@@ -3,18 +3,32 @@ package fr.thomapolis.poliscore;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import database.SqlConnection;
+
 public class PolisCore extends JavaPlugin {
+	
+	public SqlConnection sql;
 	
 	private String prefix;
 	
 	@Override
 	public void onEnable() {
 		
+		//Init
+		sql = new SqlConnection("jdbc://", "localhost", "servermc", "root", "");
+		sql.connect();
+		
 		setPrefix(ChatColor.GRAY+"["+ChatColor.RED+"PolisCore"+ChatColor.GRAY+"] ");
 		
 		System.out.println(getPrefix()+"PolisCore activate");
 		
 		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+		sql.disconnect();
 	}
 	
 	public String getPrefix() {
