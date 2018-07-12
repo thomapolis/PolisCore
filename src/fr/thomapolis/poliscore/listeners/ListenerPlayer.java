199b003826b39116fec308074317761cb73c3844
thomapolis <thomapolis@gmail.com>
2018-07-12 19:57:55 +0200

    Work on player listener

--- src/fr/thomapolis/poliscore/listeners/ListenerPlayer.java
+++ src/fr/thomapolis/poliscore/listeners/ListenerPlayer.java
@@ -1,13 +1,27 @@
 package fr.thomapolis.poliscore.listeners;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 import fr.thomapolis.poliscore.PolisCore;
+import fr.thomapolis.poliscore.database.SqlConnection;
 
 public class ListenerPlayer implements Listener {
 
+	public SqlConnection sql;
+	
 	public ListenerPlayer(PolisCore polisCore) {
 
+		this.sql = polisCore.getSQL();
+		
 	}
 
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		
+		sql.createAccount(event.getPlayer());
+		
+	}
+	
 }
