9503d5abf061e1724a4c5013b3ee404001190b3c
thomapolis <thomapolis@gmail.com>
2018-07-13 22:31:03 +0200

    Work on PolisPlayer

--- src/fr/thomapolis/poliscore/polisclass/PolisPlayer.java
+++ src/fr/thomapolis/poliscore/polisclass/PolisPlayer.java
@@ -1,5 +1,31 @@
 package fr.thomapolis.poliscore.polisclass;
 
+import org.bukkit.entity.Player;
+
+import fr.thomapolis.poliscore.PolisCore;
+import fr.thomapolis.poliscore.ranks.Rank;
+
 public class PolisPlayer {
 
+	private Player player;
+	private int id;
+	private String uuid;
+	private int money;
+	private Rank rank;
+	
+	public PolisPlayer(Player player, PolisCore core) {
+		
+		this.player = player;
+		this.id = core.getSQL().getId(player);
+		this.uuid = player.getUniqueId().toString();
+		this.money = core.getSQL().getMoney(player);
+		this.rank = core.getRankType().getRank(core.getSQL().getRank(player));
+		
+	}
+	
+	public int getMoney() {
+		
+		return this.money;
+	}
+	
 }
